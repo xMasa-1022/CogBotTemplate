580b091532e961cbda6e75e8c2da310bbff38c3f
Masa <75792981+xMasa-1022@users.noreply.github.com>
2022-06-10 02:05:13 +0900

    AdminCogの変更

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,40 +6,35 @@ import config as cf
 cogs = cf.cogs
 owners = cf.owners
 
-def restart_bot(): 
-    os.execv(sys.executable, ['python'] + sys.argv)
-
 class AdminCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="eval")
+    @commands.is_owner()
     async def _eval(self, ctx: commands.Context, *, code: str):
-        if ctx.author.id in owners:
-            if code.startswith("```python"):
-                code = code[9:-3]
-            if code.startswith("```"):
-                code = code[3:-3]
-            if code.startswith("```py"):
-                code = code[5:-3]
-            local_variables = {
-            "discord": discord,
-            "self": self,
-            "bot": self.bot,
-            "client": self.bot,
-            "ctx": ctx,
-            "message": ctx.message,
-            "author": ctx.message.author,
-            "guild": ctx.message.guild,
-            "channel": ctx.message.channel
-            }
-            try:
-                await aexec(code, local_variables)
-            except:
-                ee = discord.Embed(title="例外が発生しました！",description=f"**エラー内容:**\n```\n{traceback.format_exc()}\n```",color=0xE74C3C)
-                await ctx.channel.send(embed=ee)
-        else:
-            return
+        if code.startswith("```python"):
+            code = code[9:-3]
+        if code.startswith("```"):
+            code = code[3:-3]
+        if code.startswith("```py"):
+            code = code[5:-3]
+        local_variables = {
+        "discord": discord,
+        "self": self,
+        "bot": self.bot,
+        "client": self.bot,
+        "ctx": ctx,
+        "message": ctx.message,
+        "author": ctx.message.author,
+        "guild": ctx.message.guild,
+        "channel": ctx.message.channel
+        }
+        try:
+            await aexec(code, local_variables)
+        except:
+            ee = discord.Embed(title="例外が発生しました！",description=f"**エラー内容:**\n```\n{traceback.format_exc()}\n```",color=0xE74C3C)
+            await ctx.channel.send(embed=ee)
 
     @_eval.error
     async def _eval_error(self, ctx, error):
@@ -47,25 +42,23 @@ class AdminCog(commands.Cog):
         await ctx.send(embed = e)
 
     @commands.command(aliases=['r', 'rl'])
+    @commands.is_owner()
     async def reload(self, ctx: commands.Context) -> None:
-        if ctx.author.id in owners:
-            fails = []
-            empty_list = []
-            for extension in cogs:
-                try:
-                    self.bot.reload_extension(f"{extension}")
-                except:
-                    if extension in fails:
-                        pass
-                    else:
-                        fails.append(extension)
-            if fails == empty_list:
-                await ctx.message.add_reaction("✅")
-            else:
-                e = discord.Embed(title = "リロード中にエラーが発生しました", description = f">>> ```\n{fails}\n```", color = 0xff0000)
-                await ctx.send(embed = e)
+        fails = []
+        empty_list = []
+        for extension in cogs:
+            try:
+                self.bot.reload_extension(f"{extension}")
+            except:
+                if extension in fails:
+                    pass
+                else:
+                    fails.append(extension)
+        if fails == empty_list:
+            await ctx.message.add_reaction("✅")
         else:
-            return
+            e = discord.Embed(title = "リロード中にエラーが発生しました", description = f">>> ```\n{fails}\n```", color = 0xff0000)
+            await ctx.send(embed = e)
 
     @reload.error
     async def reload_error(self, ctx, error):
@@ -73,22 +66,18 @@ class AdminCog(commands.Cog):
         await ctx.send(embed = e)
 
     @commands.command(aliases=['shutdown'])
+    @commands.is_owner()
     async def stop(self, ctx: commands.Context) -> None:
-        if ctx.author.id in owners:
-            e = discord.Embed(title="ボット停止", description=f">>> 実行者： {ctx.author.mention} ({ctx.author})\n実行したチャンネル： {ctx.channel.mention}\n実行した時間： {datetime.datetime.now()}", color=0xff0000)
-            await ctx.send(embed=e)
-            await self.bot.close()
-        else:
-            return
+        e = discord.Embed(title="ボット停止", description=f">>> 実行者： {ctx.author.mention} ({ctx.author})\n実行したチャンネル： {ctx.channel.mention}\n実行した時間： {datetime.datetime.now()}", color=0xff0000)
+        await ctx.send(embed=e)
+        await self.bot.close()
 
     @commands.command(aliases = ["relogin", "res"])
+    @commands.is_owner()
     async def restart(self, ctx: commands.Context) -> None:
-        if ctx.author.id in owners:
-            e = discord.Embed(title = "再起動", description = f">>> 実行者： {ctx.author.mention} ({ctx.author})\n実行したチャンネル： {ctx.channel.mention}\n実行した時間： {datetime.datetime.now()}", color = 0xff0000)
-            await ctx.send(embed = e)
-            restart_bot()
-        else:
-            return
+        e = discord.Embed(title = "再起動", description = f">>> 実行者： {ctx.author.mention} ({ctx.author})\n実行したチャンネル： {ctx.channel.mention}\n実行した時間： {datetime.datetime.now()}", color = 0xff0000)
+        await ctx.send(embed = e)
+        os.execv(sys.executable, ['python'] + sys.argv)
 
 def setup(bot):
     bot.add_cog(AdminCog(bot))
